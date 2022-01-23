f306c90b3fbbf30bfac45d0b683ca37a1d1dfb58
iPottedFly <82275496+iPottedFly@users.noreply.github.com>
2022-01-22 18:27:57 -0600

    Upload another files

---
+++ Js/Countdown.js
@@ -0,0 +1,35 @@
+simplyCountdown('#contador', {
+    year: 2022, // required
+    month: 1, // required
+    day: 24, // required
+    hours: 13, // Default is 0 [0-23] integer
+    minutes: 30, // Default is 0 [0-59] integer
+    seconds: 0, // Default is 0 [0-59] integer
+    words: { //words displayed into the countdown
+        days: { singular: 'Día', plural: 'Días' },
+        hours: { singular: 'Hora', plural: 'Horas' },
+        minutes: { singular: 'Minuto', plural: 'Minutos' },
+        seconds: { singular: 'Segundo', plural: 'Segundos' }
+    },
+    plural: true, //use plurals
+    inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+    inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+    // in case of inline set to false
+    enableUtc: true, //Use UTC or not - default : false
+    onEnd: function() { 
+        document.getElementById('portada').classList.add('oculta')
+        return; }, //Callback on countdown end, put your own function here
+    refresh: 1000, // default refresh every 1s
+    sectionClass: 'simply-section', //section css class
+    amountClass: 'simply-amount', // amount css class
+    wordClass: 'simply-word', // word css class
+    zeroPad: false,
+    countUp: false
+});
+
+// Also, you can init with already existing Javascript Object.
+let myElement = document.querySelector('.my-countdown');
+simplyCountdown(myElement, { /* options */ });
+
+let multipleElements = document.querySelectorAll('.my-countdown');
+simplyCountdown(multipleElements, { /* options */ });
